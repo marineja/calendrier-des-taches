cb5735da5090fe0dbc90699e92afd2434ad96353
marineja <marine.jalabert@laposte.net>
2021-04-18 23:15:13 +0200

    message d'erreur formulaire

---
+++ identification.js
@@ -0,0 +1,26 @@
+document.getElementById("inscription").addEventListener("submit", function(e) {
+   
+
+    var erreur;
+    var pseudo = document.getElementById("pseudo");
+    var mdp = document.getElementById("mdp");
+
+    if (!pseudo.value) {
+        erreur = "Veuillez renseigner un identifiant";
+    }
+    if (!mdp.value) {
+        erreur = "Veuillez renseigner un mot de passe";
+    }
+
+    if (erreur) {
+        e.preventDefault();
+        document.getElementById("erreur").innerHTML = erreur;
+        return false;
+    } else {
+        alert('Formulaire envoyé');
+
+    }
+
+
+    
+})
